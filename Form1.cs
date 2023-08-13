1a09547f67ee1589e216d7b13fbe60ebfd1a0f90
Ricardo Gunawan <ricardogunawanp@live.com>
2023-08-13 17:50:00 +0700

    Source commit and Create Readme

---
+++ Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleToDo7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addToDo()
+        {
+            var todotext = textBox1.Text;
+            listBox1.Items.Add(todotext);
+            resetTextbox1();
+        }
+
+        private void editToDo()
+        {
+            var editedToDo = textBox1.Text;
+            listBox1.Items[listBox1.SelectedIndex] = editedToDo;
+        }
+
+        public void resetAfterEdit()
+        {
+            // call reset button function
+            resetTextbox1();
+
+            // reset the rest:
+            // rename buttons
+            button6.Text = "Edit";
+            button1.Text = "Add ToDo!";
+
+            // enable the rest of buttons
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button7.Enabled = true;
+
+            // enable listbox
+            listBox1.Enabled = true;
+        }
+
+        private void resetTextbox1()
+        {
+            textBox1.Text = "";
+        }
+
+        private void resetStatusStrip()
+        {
+            toolStripStatusLabel1.Text = "Standby";
+            toolStripProgressBar1.Value = 0;
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resetTextbox1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset if button is edit state
+            if (button1.Text.Contains("Edit"))
+            {
+                // functon to Edit
+                editToDo();
+
+                // reset all button states
+                resetAfterEdit();
+                return;
+            }
+
+            if (textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Please insert ToDo first", "Error: ToDo empty", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            addToDo();
+            
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button5_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button6_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button7_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Clear the add ToDo textbox";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Add what you just type to the ToDo list!";
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Reorder the selected ToDo, up by 1 level";
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Reorder the selected ToDo, down by 1 level";
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Delete the selected ToDo";
+        }
+
+        private void button6_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Edit the selected ToDo";
+        }
+
+        private void button7_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Mark selected ToDo as completed!";
+        }
+
+        private void clearAllToDosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+            {
+                MessageBox.Show("There are no ToDo(s) yet!", "Nothing to clear!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            var ask = MessageBox.Show("Are you sure you want to clear all ToDos?\nThis action can't be undoned!", "Clear All Items", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (ask.Equals(DialogResult.Yes))
+            {
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void clearAllToDosToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Caution: Clear all ToDos";
+        }
+
+        private void clearAllToDosToolStripMenuItem_MouseLeave(object sender, EventArgs e)
+        {
+            resetStatusStrip();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show(this, "The ToDo List is Empty..\nNothing to save!", "Oops!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+            toolStripStatusLabel1.Text = "Saving ToDo to .txt ... ";
+            toolStripProgressBar1.Visible = true;
+            toolStripProgressBar1.Value = 25;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Header file
+                toolStripProgressBar1.Value = 30;
+                var enter = Environment.NewLine;
+                var separator = "----------------------------------------";
+                var header = "----- Simple ToDo7 Saved ToDo List -----";
+                toolStripProgressBar1.Value = 50;
+                File.WriteAllText(saveFileDialog1.FileName, separator + enter + header + enter + separator + enter + enter);
+                toolStripProgressBar1.Value = 75;
+                foreach (var item in listBox1.Items)
+                {
+                    var bullet = "o ";
+                    if (item.ToString().Contains("Completed")) bullet = "- ";
+                    File.AppendAllText(saveFileDialog1.FileName, bullet + item.ToString() + enter);
+                }
+                toolStripProgressBar1.Value = 100;
+
+                MessageBox.Show(this, "ToDo List saved succesfully!", "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                resetStatusStrip();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show(this, "Please select a ToDo to be deleted!", "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            var ask = MessageBox.Show("Are you sure want to delete this ToDo?",
+                "Delete Confirmation",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (ask.Equals(DialogResult.Yes))
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                MessageBox.Show(this, "ToDo Deleted!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show(this, "Select a Todo first!", "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            if (listBox1.Items[listBox1.SelectedIndex].ToString().Contains("Completed!"))
+            {
+                MessageBox.Show(this, "Oops! ToDo is already completed!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning
+                );
+                return;
+            }
+            var selectedTodo = listBox1.Items[listBox1.SelectedIndex].ToString();
+            var completedTodo = "(" + selectedTodo + ") --Completed!";
+            listBox1.Items[listBox1.SelectedIndex] = completedTodo;
+            
+            MessageBox.Show(this, "ToDo Completed!", "Success",
+                MessageBoxButtons.OK, MessageBoxIcon.Information
+            );
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // if this button is for cancel edit:
+            if (button6.Text.Contains("Cancel"))
+            {
+                // call reset after edit
+                resetAfterEdit();
+
+                // return
+                return;
+            }
+            int selectedIndex = 0;
+            var oldValue = "";
+
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show(this, "Select a Todo to edit!", "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            if (listBox1.Items[listBox1.SelectedIndex].ToString().Contains("Completed!"))
+            {
+                MessageBox.Show(this, "Oops! ToDo is already completed and can't be edited!", "Can't edit completed ToDo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            // process selected data
+            selectedIndex = listBox1.SelectedIndex;
+            oldValue = listBox1.Items[listBox1.SelectedIndex].ToString();
+
+            /*
+             * Controlling The Forms
+             * 
+             * Disabling items, buttons, etc.
+             * 
+             */
+
+            // put old value to textbox
+            textBox1.Text = oldValue;
+
+            // rename buttons
+            button6.Text = "Cancel Edit";
+            button1.Text = "Edit ToDo!";
+
+            // disable the rest of buttons
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            //button6.Enabled = false;
+            button7.Enabled = false;
+
+            // disable listbox
+            listBox1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+            {
+                return;
+            }
+            
+            int currPosition;
+            currPosition = listBox1.SelectedIndex;
+            
+            // can't move if already on top
+            if (currPosition == 0)
+            {
+                MessageBox.Show(this, "Oops! ToDo is already on Top!", "Can't move ToDo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            var tempVal = listBox1.Items[currPosition];
+
+            // moving process
+            listBox1.Items[currPosition] = listBox1.Items[currPosition - 1];
+            listBox1.Items[currPosition - 1] = tempVal;
+            listBox1.SelectedIndex = (currPosition - 1);
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+            {
+                return;
+            }
+
+            int currPosition;
+            currPosition = listBox1.SelectedIndex;
+
+            // can't move if already on top
+            if (currPosition == listBox1.Items.Count-1)
+            {
+                MessageBox.Show(this, "Oops! ToDo is already on Buttom!", "Can't move ToDo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            var tempVal = listBox1.Items[currPosition];
+
+            // moving process
+            listBox1.Items[currPosition] = listBox1.Items[currPosition + 1];
+            listBox1.Items[currPosition + 1] = tempVal;
+            listBox1.SelectedIndex = (currPosition + 1);
+
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var aboutForm = new About();
+            aboutForm.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var x = MessageBox.Show("All ToDos will be lost after you exit!\nAre you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (x.Equals(DialogResult.Yes))
+            {
+                return;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void featuresComingSoonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Features Coming Soon!", "Coming Soon!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void preferencesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Features Coming Soon!", "Coming Soon!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Simple Guide\n"
+            + "\nView the status bar for hint about the buttons usages."
+            + "\nSimply type your first thing you want to do in the textbox, and click \"Add ToDo!\""
+            + "\nTo Complete or Delete a ToDo, simply click on the appropriate buttons."
+            + "\nTo Edit a ToDo, simply click Edit Button, change the value, and click \"Edit ToDo!\" button."
+            , "Simple Guide", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# SimpleToDo7
-Simple ToDo Application made natively for Windows 7
+# Simple ToDo7
+![SimpleToDo7Logo](images/ToDoSeven.png)
+
+<i>Simple ToDo7 Logo, the 7 is inspired from Microsoft To-Do logo</i>
+
+Simple ToDo7 is a To-Do Application made natively for Windows 7. The main purpose is to help you have a handy yet simple to-do application for your Windows 7 environtment. The second purpose is to help inspire new or learner programmers to study or create a simple yet useful utility applications.
+
+(Note: See screenshots below)
+
+## Background
+
+I'm currently having nostalgic moments by installing old operating systems in a Virtual Machine and using them as my daily OS. While using Windows Vista, I created "ToDo Vista" which you can see [here](https://github.com/ricardo1pran/ToDoVista). After a while in Windows Vista exploring the features I've known and neven known before, it's time for me to move to Windows 7. But now I feel incomplete, and wanted to improve my ToDo Vista better. But because now I'm using Windows 7, it's time to create the better version as a separate application.
+
+## Features
+
+I call a task in this application as a "ToDo". Anything mentioned as "ToDo" inside the app, means the task. There are features that's already exist in ToDo Vista tho, so I'll make the features that only available in Simple ToDo7 as bold. See the list of the features below:
+
+- Add ToDo
+- Complete ToDo
+- Delete ToDo
+- <b>Edit existing ToDo</b>
+- Save ToDo list to a .txt file
+- <b>Clear existing ToDo list</b>
+- <b>Move ToDo items in the list up and down</b>
+- <b>A simple How To Guide in Help context menu</b>
+- <b>See completed ToDo as separate list</b> (upcoming feature!)
+- <b>Set preferences</b> (upcoming feature!)
+
+<i>The preferences will be able to set some options such as toggle Skip Completed ToDo in Txt, toggle Set Completed ToDo always on buttom, etc.</i>
+
+## Technical Requirements
+
+There are no specific requirements for this application, I believe it will run on Windows 7 and newer machines, so I'll just mention the tools I used to create this application.
+
+- Microsoft Visual C# Express 2010
+- Microsoft .NET 4.5
+
+## Application Support and Updates
+
+I'll still be in Windows 7 operating system until September 2023, so expect the "upcoming" features to be done before September 30th. After that I'll move to Windows 8.1, and maybe create a newer modern ToDo application.
+
+## Usage
+
+As stated on the license I chose. Plus you're free to contact me [here](contact@ricardogunawan.com) if you think you want to use my apps for other purposes. Usage for study purposes are always welcome.
+
+## Screenshots
+
+![todo](images/1.png)<br/>
+<i>Simple ToDo7 Main App View</i>
+
+![todo](images/2complete.png)<br/>
+<i>Simple ToDo7 Complete a ToDo</i>
+
+![todo](images/3edit.png)<br/>
+<i>Simple ToDo7 edit a ToDo</i>
+
+![todo](images/4del.png)<br/>
+<i>Simple ToDo7 delete a ToDo</i>
+
+![todo](images/5-1-save.png)<br/>
+<i>Simple ToDo7 Save as menu</i>
+
+![todo](images/5-2-save.png)<br/>
+<i>Simple ToDo7 Save as dialog</i>
+
+![todo](images/5-3-save.png)<br/>
+<i>Simple ToDo7 saved .txt file</i>
+
+![todo](images/6cls.png)<br/>
+<i>Simple ToDo7 Clear ToDo (confirmation)</i>
+
+![todo](images/about.png)<br/>
+<i>Simple ToDo7 v.1.0.0 New Modern and Clean About (compared to ToDo Vista)</i>
